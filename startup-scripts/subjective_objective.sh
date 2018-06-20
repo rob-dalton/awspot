1863fe7d802c13040189efa0c4dc26c7b3483c93
Rob Dalton <rob-dalton@users.noreply.github.com>
2018-06-19 21:32:49 -0700

    Updated startup script.

--- startup-scripts/subjective_objective.sh
+++ startup-scripts/subjective_objective.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
+# install git
 sudo yum -y install git-all
 
+# install project dependencies
+conda install -y numpy
+conda install -y pandas
+
+# clone project repo
 cd /home/ec2-user
 git clone https://github.com/rob-dalton/subjective-objective-classifier.git
 
+# run project
 cd subjective-objective-classifier
 make data
