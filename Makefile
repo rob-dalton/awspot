1db4ba95c3f8c5b8932fb8cd218b52a8b93b5447
Rob Dalton <rob-dalton@users.noreply.github.com>
2018-08-01 22:37:32 -0700

    Changed file structure

--- Makefile
+++
@@ -1,6 +0,0 @@
-install:
-	python generate_script.py
-
-package:
-	tar --exclude='./.git' --exclude='./awspot' --exclude='./specifications' \
-			--exclude='./userdata-scripts' -zcvf "awspot-0.1.tar.gz" .
--- setup.py
+++ setup.py
@@ -1,12 +1,20 @@
+#!/usr/bin/env python
 from distutils.core import setup
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
 
 setup(
     name='AWSpot',
-    version='0.1.0',
+    version='0.0.3',
     author='Rob Dalton',
     author_email='rob@robdalton.me',
+    classifiers=(
+        'Programming Language :: Python :: 3.6'
+    ),
     packages=['awspot', 'awspot.managers'],
-    scripts=['bin/ec2.py'],
+    scripts=['bin/awspot'],
     url='http://pypi.python.org/pypi/AWSpot/',
     license='LICENSE',
     description='Utility for managing AWS spot resources.',
