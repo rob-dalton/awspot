43b7c84d2047a5205406609f7f2df4c921724ca9
Rob Dalton <rob-dalton@users.noreply.github.com>
2018-09-14 19:56:39 -0700

    Changed setup.py to use setuptools instead of distutils

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 def readme():
     with open('README.md') as f:
@@ -7,19 +7,19 @@ def readme():
 
 setup(
     name='AWSpot',
-    version='0.0.3',
+    version='0.0.4',
     author='Rob Dalton',
     author_email='rob@robdalton.me',
     python_requires='>=3.6',
-    classifiers=(
-        'Programming Language :: Python :: 3.6'
-    ),
-    packages=['awspot', 'awspot.managers'],
+    classifiers=[
+        "Programming Language :: Python :: 3.6"
+    ],
+    packages=find_packages(),
     scripts=['bin/awspot'],
     url='http://pypi.python.org/pypi/AWSpot/',
     license='LICENSE',
     description='Utility for managing AWS spot resources.',
-    long_description=open('README.md').read(),
+    long_description=open('README').read(),
     install_requires=[
         "boto3 >= 1.7.24"
     ],
